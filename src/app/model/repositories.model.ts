749ed121e82fd3750728f9e95aa6e95bb452b4b2
unknown <beerbaron322@gmail.com>
2020-08-05 19:59:56 +0300

    Add github api authorization

---
+++ src/app/model/repositories.model.ts
@@ -0,0 +1,9 @@
+export class RepositoriesModel {
+    private baseUrl: string = 'https://api.github.com';
+    private token = require('../../../token.json').token;
+
+    constructor() {
+        
+    }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 import './assets/scss/main.scss';
+import {RepositoriesModel} from './app/model/repositories.model';
+
+let a = new RepositoriesModel();
