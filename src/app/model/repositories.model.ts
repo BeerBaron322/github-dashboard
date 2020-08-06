f202e0b4b8c72126465bd328da778bbd3475eb1b
unknown <beerbaron322@gmail.com>
2020-08-06 17:55:30 +0300

    Get repositories with options(name, stars, page).

--- src/app/model/repositories.model.ts
+++ src/app/model/repositories.model.ts
@@ -1,9 +1,33 @@
 export class RepositoriesModel {
-    private baseUrl: string = 'https://api.github.com';
+    public baseUrl: string = 'https://api.github.com';
     private token = require('../../../token.json').token;
+    private headers: Headers | string[][] | Record<string, string> | undefined = {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${this.token}`,
+    }
 
     constructor() {
         
     }
 
+    public getRepoByName(name: string, page?: number) {
+        let queryParametrs: string = `?q=${name}in:name&per_page=10`;
+        if (page) {
+            queryParametrs += `&page=${page}`;
+        }
+        let request = fetch(`${this.baseUrl}/search/repositories${queryParametrs}`, {
+            method: 'GET',
+            headers: this.headers
+        });
+        return request.then(response => response.json());
+    }
+
+    public getRepoByStars() {
+        let queryParametrs: string = '?q=stars:>100000&git per_page=10'
+        let request = fetch(`${this.baseUrl}/search/repositories${queryParametrs}`, {
+            method: 'GET',
+            headers: this.headers
+        });
+        return request.then(response => response.json());
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,19 @@ import './assets/scss/main.scss';
 import {RepositoriesModel} from './app/model/repositories.model';
 
 let a = new RepositoriesModel();
+
+a.getRepoByName('jquery').then((data:any) => {
+    console.log('all data: ',data);
+});
+a.getRepoByName('jquery', 1).then((data:any) => {
+    console.log('first page:', data);
+});
+a.getRepoByName('jquery', 2).then((data:any) => {
+    console.log('second page:', data);
+});
+
+a.getRepoByStars().then((data:any) => {
+    console.log(data);
+});
+
+
